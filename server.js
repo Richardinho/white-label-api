0cca5a680a4a38ca19b23ff81e25f22417aab946
Richard Hunter <richard@richardhunter.co.uk>
2016-08-21 17:23:36 +0100

    first proper commit

---
+++ server.js
@@ -0,0 +1,142 @@
+
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var defaultParams = {
+	'year-from' : 50,
+	'year-to'   : 300,
+	'dynasty'   : 'Flavian',
+	'sort-by'   : 'succession'
+};
+
+var EMPERORS_FILE = path.join(__dirname, 'emperors.json');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use('/', express.static(path.join(__dirname, 'web')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+	// Set permissive CORS header - this allows this server to be used only as
+	// an API server in conjunction with something like webpack-dev-server.
+	res.setHeader('Access-Control-Allow-Origin', '*');
+
+	// Disable caching so we'll always get the latest comments.
+	res.setHeader('Cache-Control', 'no-cache');
+	next();
+});
+
+app.get('/api/emperor', function(req, res) {
+	setTimeout(function () {
+		res.sendFile(createEmperorFileName(req.param('id')));
+	}, 1000); //  choking response time
+});
+
+function createEmperorFileName (id) {
+	return path.join(__dirname, 'emperors/' + id + '.html');
+}
+
+app.get('/api/emperors', function(req, res) {
+	fs.readFile(EMPERORS_FILE, function(err, data) {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		var json = JSON.parse(data);
+
+		var dynastyFilterOption = getDynastyFilterOption(req);
+		var yearFrom = getYearFrom(req);
+		var yearTo = getYearTo(req);
+		var sortOption = getSortOption(req);
+
+		var filtered = filterBy(json, dynastyFilterOption, yearFrom, yearTo);
+
+		setTimeout(function () {
+			res.json({
+				results : sortBy(filtered, sortOption),
+				criteria : {
+					'dynasty' : dynastyFilterOption,
+					'yearFrom' : yearFrom,
+					'yearTo' : yearTo,
+					'sortBy' : sortOption,
+					minYear : -50,
+					maxYear : 400,
+					dynasties : [
+						'all',
+						'Julio-Claudian',
+						'Flavian',
+						'Nerva-Antonine',
+						'Severan',
+						'Gordian'
+					],
+					sortingOrders : [
+						'reign-asc',
+						'reign-desc',
+						'succession'
+					]
+				}
+			});
+		}, 1000); //  choking response time
+	});
+});
+
+function getYearFrom(request) {
+	var yearFrom = request.param('year-from');
+	return yearFrom ? yearFrom : defaultParams['year-from'];
+}
+
+function getYearTo(request) {
+	var yearTo = request.param('year-to');
+	return yearTo ? yearTo : defaultParams['year-to'];
+}
+
+function getSortOption (request) {
+	var sortBy = request.param('sort-by');
+	return sortBy ? sortBy : 'succession';
+}
+
+function getDynastyFilterOption(request) {
+	var dynasty = request.param('dynasty');
+	return dynasty ? dynasty : defaultParams['dynasty']; // default for the moment
+}
+
+function filterBy(data, dynasty, from, to) {
+
+	data = data.filter(function(emperor){
+		return emperor.to > from && emperor.from < to;
+	});
+
+	return data.filter(function(emperor) {
+		if(dynasty == 'all') return true;
+		return emperor.dynasty == dynasty;
+	});
+}
+
+function sortBy(data, sortOption) {
+	if(sortOption == 'reign-asc') {
+		return data.sort(function (a,b) {
+			return getReign(a) - getReign(b);
+		});
+	} else if(sortOption == 'reign-desc'){
+		return data.sort(function (a,b) {
+			return getReign(b) - getReign(a);
+		});
+	} else { //  succession
+		return data.sort(function (a,b) {
+			return a.from - b.from;
+		});
+	}
+}
+
+function getReign(emperor) {
+	return emperor.to - emperor.from;
+}
+
+app.listen(app.get('port'), function() {
+	console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
